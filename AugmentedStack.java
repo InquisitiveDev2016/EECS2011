85b96adfcaaf0dcb79077449bbe98f86505c1672
vishal0027 <vishal27@my.yorku.ca>
2016-10-10 17:16:02 -0400

    Update AugmentedStack.java

--- AugmentedStack.java
+++ AugmentedStack.java
@@ -5,7 +5,10 @@
  * Student cse account: vishal27
  * Student ID number: 214537146
  **********************************************************/
-
+/**
+ * This is an edited stack which return minimum element of stack in O(1) time.
+ * @author vishal
+ */
 import java.util.Stack;
 public class AugmentedStack{
 
@@ -15,26 +18,46 @@ public class AugmentedStack{
 		this.stack= new Stack<Double>();
 		this.minStack= new Stack<Double>();
 	}
-	
+	/**
+	 * This is an method which adds elements into stack and minstack. it initially adds the first value of element as it is then checks for 
+	 * other values if the values are smaller then the minStack.peek() element,if it is then it will add that element into minStack. So that
+	 *  minStack only have useful element instead of multiple copies of same element over an over again.
+	 * @param element
+	 */
 	public void push(double element){	
 		stack.push(element);
 		if (minStack.empty() || element <= minStack.peek()){
 		      minStack.push(element);
 		  }
 	}
+	/**
+	 * This method remove the recent  element added into stack. This is a special method since it also remove elements to MinStack as well.
+	 * to add initially it checks wheather the stack.peek() is same to the minStack or not, if its similar then it pops the minStack element
+	 *   
+	 * @return stack.pop(); + minStack.pop();
+	 */
 	
 	public Double pop(){
+		if(stack.isEmpty()){
+			return null;
+		}
 	    double value = stack.peek();
 		if(value == minStack.peek()){
 			minStack.pop();
 		}
 		return stack.pop();
 	}
-	
+	/**
+	 * This is the method which return top element of stack
+	 * @return stack.peek();
+	 */
 	public Double top(){
 		return this.stack.peek();
 	}
-	
+	/**
+	 * This is a method which return the minimum value of stack by poping value from minStack.
+	 * @return
+	 */
 	public Double getMin(){
 		if(minStack.isEmpty()){
 			return null;
@@ -44,32 +67,30 @@ public class AugmentedStack{
 	}
 	@Override
 	public String toString() {
-		return "Augmented Stack = "+ stack+"\n" +"Minimum = " + getMin();
+		return "Augmented Stack = "+ stack+"\n"+/*"MinStack = "+minStack+*/"\n" +"Minimum = " + getMin();
 	}
+	/**
+	 * This is main method which runs some test on Augmented Stack.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Test Cases
 		AugmentedStack a  = new AugmentedStack();
-		a.push(48);
+		a.push(90);
 		a.push(9);
 		a.push(100);
+		a.push(3);
 		a.push(28);
 		a.push(18283);
-		a.push(3);
-		a.push(89);
 		a.push(-81);
-		a.push(-11);
-
-	/*	a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
-		a.pop();
+		a.push(89);
+		a.push(-11);  
 		
+		
+		/*a.pop();
+		  a.pop();
+		  a.pop();
 		*/
 		System.out.println(a);
 	}
